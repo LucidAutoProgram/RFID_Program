e992acef806f60c2041f75f61cde62e81912acee
heerachander55 <heerachander55@gmail.com>
2024-02-28 11:38:41 -0500

    lights working

---
+++ func.py
@@ -0,0 +1,218 @@
+import platform
+import subprocess
+from typing import Tuple, List
+
+import mysql.connector
+from eventlet import tpool
+from mysql.connector import Error
+
+
+class DatabaseOperations:
+    def __init__(self, host_ip, host_username, db_password, db_name, db_ip, db_port, db_pool_name, db_pool_size):
+        self.host_ip = host_ip
+        self.host_username = host_username
+        self.db_password = db_password
+        self.db_name = db_name
+        self.db_ip = db_ip
+        self.db_port = db_port
+        self.db_pool_name = db_pool_name
+        self.db_pool_size = db_pool_size
+        self.connection = self.create_server_connection()  # Establish connection at initialization
+
+    def create_server_connection(self):
+        try:
+            connection = mysql.connector.connect(
+                host=self.host_ip,
+                user=self.host_username,
+                passwd=self.db_password,
+                database=self.db_name,
+                port=self.db_port
+            )
+            print("MySQL Database connection successful")
+            return connection
+        except Error as err:
+            print(f"Error Making DataBase Connection to {self.db_ip}: '{err}'")
+            return None
+
+    def findAllDeviceIPInRFIDDeviceDetails(self) -> List[Tuple[str]]:
+        """
+           Fetches all the Device_IP from the RFID_Device_Details table.
+
+        :return: List[Tuple[
+                            Device_IP
+                    ]]
+        """
+        db_cursor = None
+        try:
+            db_cursor = self.connection.cursor()
+            prepared_statement = """
+                                    SELECT Device_IP 
+                                    FROM RFID_Device_Details 
+                                  """
+            db_cursor.execute(prepared_statement)
+            db_result = db_cursor.fetchall()  # Get query results
+            return db_result
+
+        except Exception as e:
+            print(f'Error from DatabaseOperations.findAllDeviceIPInRFIDDeviceDetails => {e}')
+        finally:
+            db_cursor.close()
+
+    def findDevicePortInRFIDDeviceDetailsUsingDeviceIP(self, device_ip) -> List[Tuple[str]]:
+        """
+            Fetches the Device Port based on its DeviceIP.
+            :param device_ip: The ip address of the rfid device.
+            :return: List[Tuple[
+                                Device_Port
+                        ]]
+        """
+        db_cursor = None
+        try:
+            db_cursor = self.connection.cursor()
+            prepared_statement = """
+                                            SELECT Device_Port 
+                                            FROM RFID_Device_Details 
+                                            WHERE Device_IP = %s
+                                          """
+            db_cursor.execute(prepared_statement, (device_ip,))
+            db_result = db_cursor.fetchall()  # Get query results
+            return db_result
+
+        except Exception as e:
+            print(f'Error from DatabaseOperations.findDevicePortInRFIDDeviceDetailsUsingDeviceIP => {e}')
+        finally:
+            db_cursor.close()
+
+    def findDeviceLocationInRFIDDeviceDetailsUsingDeviceIP(self, device_ip) -> List[Tuple[str]]:
+        """
+            Fetches the Device Location based on its DeviceIP.
+            :param device_ip: The ip address of the rfid device.
+            :return: List[Tuple[
+                                Location
+                        ]]
+        """
+        db_cursor = None
+        try:
+            db_cursor = self.connection.cursor()
+            prepared_statement = """
+                                            SELECT Location 
+                                            FROM RFID_Device_Details 
+                                            WHERE Device_IP = %s
+                                          """
+            db_cursor.execute(prepared_statement, (device_ip,))
+            db_result = db_cursor.fetchall()  # Get query results
+            return db_result
+
+        except Exception as e:
+            print(f'Error from DatabaseOperations.findDeviceLocationInRFIDDeviceDetailsUsingDeviceIP => {e}')
+        finally:
+            db_cursor.close()
+
+    def findReadingModeInRFIDDeviceDetailsUsingDeviceIP(self, device_ip) -> List[Tuple[str]]:
+        """
+            Fetches the reading mode based on its DeviceIP, whether it is on or off.
+            :param device_ip: The ip address of the rfid device.
+            :return: List[Tuple[
+                                Reading_Mode
+                        ]]
+        """
+        db_cursor = None
+        try:
+            db_cursor = self.connection.cursor()
+            prepared_statement = """
+                                            SELECT Reading_Mode 
+                                            FROM RFID_Device_Details 
+                                            WHERE Device_IP = %s
+                                          """
+            db_cursor.execute(prepared_statement, (device_ip,))
+            db_result = db_cursor.fetchall()  # Get query results
+            return db_result
+
+        except Exception as e:
+            print(f'Error from DatabaseOperations.findReadingModeInRFIDDeviceDetailsUsingDeviceIP => {e}')
+        finally:
+            db_cursor.close()
+
+
+server_connection_params = DatabaseOperations(
+    host_ip='192.168.10.1',
+    host_username='LucidAuto',
+    db_password='Lucid@390',
+    db_name='LucidAutoDB',
+    db_ip='192.168.10.1',
+    db_port=3306,
+    db_pool_name='server_db_pool',
+    db_pool_size=5
+)
+
+device_ips = server_connection_params.findAllDeviceIPInRFIDDeviceDetails()
+if device_ips:
+    print(f"Device IPs: {device_ips}")
+    for ip in device_ips:
+        device_port = server_connection_params.findDevicePortInRFIDDeviceDetailsUsingDeviceIP(ip[0])
+        print(f'Device Port for {ip[0]} - {device_port}')
+        device_location = server_connection_params.findDeviceLocationInRFIDDeviceDetailsUsingDeviceIP(ip[0])
+        print(f'Device Location for {ip[0]} is {device_location}')
+        reading_mode = server_connection_params.findReadingModeInRFIDDeviceDetailsUsingDeviceIP(ip[0])
+        print(f'Reading mode for {ip[0]} is {reading_mode}')
+else:
+    print("Device IPs could not be found or an error occurred.")
+
+
+# ------------------------ Function for checking the status of rfid reader whether it is online or not.----------------
+
+def rfid_connectivity_checker(ip_address, number_of_attempts: int = 2):
+    """
+        Offloads a pinging operation
+        Sets fail threshold to 2 attempts
+
+        :param ip_address: The IP to ping
+        :param number_of_attempts: The amount of times to ping before giving up. Default is 2 attempts.
+
+        :return: True if the ping is successful within the given attempts, False otherwise.
+    """
+    # Determine the number of pings based on the operating system
+    if platform.system().lower() == 'windows':
+        param = f'-n {number_of_attempts}'  # Windows uses '-n' for count
+    else:
+        param = f'-c {number_of_attempts}'  # Linux and macOS use '-c' for count
+    command = f'ping {param} {ip_address}'
+
+    # Run the ping command in a tpool to prevent blocking
+    process = tpool.execute(
+        subprocess.Popen,
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    # Wait for the ping command to complete
+    process.wait()
+    return process.returncode == 0
+
+# def continuous_check(device_ips, check_interval=10):
+#     """
+#     Continuously checks the connectivity of a list of IP addresses.
+
+#     :param device_ips: A tuple containing the IP addresses.
+#     :param check_interval: The interval between checks for each IP address, in seconds.
+#     """
+#     def check_ip(ip):
+#         """
+#             Checks the connectivity of a single IP address and prints the result.
+#         """
+#         is_online = rfid_connectivity_checker(ip)
+#         status = "online" if is_online else "offline"
+#         print(f"IP {ip} is {status}")
+
+#     while True:
+#         print("Starting connectivity check...")
+#         pool = GreenPool()
+#         for ip in device_ips:
+#             ip_address = ip
+#             pool.spawn_n(check_ip, ip_address)
+
+#         pool.waitall()  # Wait for all checks to complete
+#         print("Connectivity check completed. Waiting for the next round...")
+#         eventlet.sleep(check_interval)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import concurrent
 import io
 from PIL import Image
-
+from func import rfid_connectivity_checker, DatabaseOperations
 import PySimpleGUI as sg
+import eventlet
+from concurrent.futures import ThreadPoolExecutor
+
+eventlet.monkey_patch()
 
 # Setting the desired width and height of image
 width = 15
@@ -20,33 +25,61 @@ def get_image_data(file, maxsize=(width, height)):
     return data
 
 
-def create_rfid_layout(rfid_reader_number):
+def create_rfid_layout(ip, status_color):
     """
-    Generating layout for each RFID box
+        Generating layout for each RFID box
     """
     return [
-        [sg.Image(data=get_image_data(f'images/red.png', maxsize=(width, height)), background_color='black'),
-         sg.Button(rfid_reader_number, button_color='black', border_width=0, focus=False,
-                   key=f'BUTTON_{rfid_reader_number}')
+        [sg.Image(data=get_image_data(f'images/{status_color}.png', maxsize=(width, height)), background_color='black',
+                  key=f'IMAGE_{ip}'),
+         sg.Button(ip, button_color='black', border_width=0, focus=False,
+                   key=f'BUTTON_{ip}')
          ],
     ]
 
 
-def launch_gui():
+def update_rfid_status(window, ip_addresses):
+    """
+    Function to update the RFID status for all IP addresses at once using ThreadPoolExecutor.
+    """
+
+    def check_and_update(ip_address):
+        is_online = rfid_connectivity_checker(ip_address)
+        print(f'Status of IP address {ip_address} is {"online" if is_online else "offline"}')
+        status_color = 'green' if is_online else 'red'
+        return ip_address, get_image_data(f'images/{status_color}.png', maxsize=(width, height))
+
+    with ThreadPoolExecutor() as executor:
+        future_to_ip = {executor.submit(check_and_update, ip): ip for ip in ip_addresses}
+        for future in concurrent.futures.as_completed(future_to_ip):
+            ip_address, image_data = future.result()
+            window[f'IMAGE_{ip_address}'].update(data=image_data)
+
+    # while True:
+    #     for ip in ip_addresses:
+    #         eventlet.spawn_n(check_and_update, ip)
+    #     eventlet.sleep(10)  # Use eventlet's sleep for non-blocking wait
+
+def launch_gui(ip_addresses):
     """
     Function to launch the GUI panel
     """
     sg.theme('DarkGrey12')
 
-    # Generating RFID layouts for different RFID_READERS
-    rfid_reader_numbers = ['INPAC#001', 'INPAC#002', 'INPAC#003']  # Add more INPAC numbers as needed
-    rfid_boxes = [sg.Frame(title='', layout=create_rfid_layout(rfid_reader_number), border_width=2, title_color='white',
-                           relief=sg.RELIEF_SUNKEN, background_color='black') for rfid_reader_number in
-                  rfid_reader_numbers]
+    # # Generating RFID layouts for different RFID_READERS
+    # rfid_reader_numbers = ['INPAC#001', 'INPAC#002', 'INPAC#003']  # Add more INPAC numbers as needed
+    # rfid_boxes = [sg.Frame(title='', layout=create_rfid_layout(rfid_reader_number), border_width=2, title_color='white',
+    #                        relief=sg.RELIEF_SUNKEN, background_color='black') for rfid_reader_number in
+    #               rfid_reader_numbers]
+    ip_add = ip_addresses
+    # Instead of creating a horizontal row for RFID boxes, create a vertical column layout
+    rfid_boxes = [[sg.Frame(title='', layout=create_rfid_layout(ip, 'red'), border_width=2,
+                            title_color='white', relief=sg.RELIEF_SUNKEN, background_color='black')] for
+                  ip in ip_add]
 
     # Adding a black box to the layout
     details_box = sg.Column([
-        [sg.Text('Additional Information', background_color='black', text_color='white', size=(20, 1))]],
+        [sg.Text('Device Details', background_color='black', text_color='white', size=(20, 1))]],
         key='DETAILS_BOX',
         background_color='black',
         visible=False,  # Initially hidden
@@ -69,16 +102,20 @@ def launch_gui():
     rfid_row = rfid_boxes
 
     layout = [
-        rfid_row,  # Adding the row of RFID boxes to main layout
+        rfid_row,  # Adding the row of RFID boxes to main layout0
         [terminal_box, details_box],
     ]
 
-    window = sg.Window(title="RFID Reader Dashboard ", layout=layout, margins=(10, 10), resizable=True)
+    window = sg.Window(title="RFID Reader Dashboard ", layout=layout, margins=(10, 10), resizable=True, finalize=True)
+
+    # Update the RFID statuses immediately after window creation
+    update_rfid_status(window, ip_addresses)
 
     last_clicked = None  # To keep track of the last clicked button
 
     while True:
         event, values = window.read()
+        print(f"Event: {event}, Values: {values}")  # Log all events and values
         if event == sg.WINDOW_CLOSED:
             break
 
@@ -98,8 +135,25 @@ def launch_gui():
                 window['DETAILS_BOX'].update(visible=True)
                 last_clicked = event  # Updating last clicked to the current button
 
+        elif event.startswith('UPDATE_STATUS_'):
+            ip = event.split('_')[-1]  # Extracting the IP from the event key
+            is_online = values[event]
+            print(f"Received update event for {ip}: {is_online}")  # Debug print before updating
+            status_color = 'green' if is_online else 'red'  # Corrected color mapping
+            print(f"Updating {ip} to {'online' if is_online else 'offline'} (color: {status_color})")  # Debug print
+            window[f'IMAGE_{ip}'].update(data=get_image_data(f'images/{status_color}.png', maxsize=(width, height)))
+
     window.close()
 
 
 if __name__ == '__main__':
-    launch_gui()
+    # Initialize database operations and fetch device IPs
+    db_connection = DatabaseOperations(
+        host_ip='192.168.10.1', host_username='LucidAuto', db_password='Lucid@390',
+        db_name='LucidAutoDB', db_ip='192.168.10.1', db_port=3306, db_pool_name='server_db_pool', db_pool_size=5
+    )
+    device_ips = [ip[0] for ip in db_connection.findAllDeviceIPInRFIDDeviceDetails()]  # This will contain the list of
+    # ip addresses stored in it.
+
+    # Start the GUI
+    launch_gui(device_ips)
